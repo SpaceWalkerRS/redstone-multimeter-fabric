cfc423c70748548c0b5354e4c0bf6fc0d0da5e3f
Space Walker <48224626+SpaceWalkerRS@users.noreply.github.com>
2021-03-15 09:12:33 +0100

    Add files via upload

---
+++ src/main/java/rsmm/fabric/RedstoneMultimeter.java
@@ -0,0 +1,11 @@
+package rsmm.fabric;
+
+import net.fabricmc.api.ModInitializer;
+
+public class RedstoneMultimeter implements ModInitializer {
+	
+	@Override
+	public void onInitialize() {
+		
+	}
+}
