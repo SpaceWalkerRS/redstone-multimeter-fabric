3b257873163605654810be774382b8e42cabb34b
Space Walker <spacedoesrs@gmail.com>
2021-04-22 03:07:51 +0200

    Fixed extra empty columns in the subtick table

--- src/main/java/rsmm/fabric/RedstoneMultimeterMod.java
+++ src/main/java/rsmm/fabric/RedstoneMultimeterMod.java
@@ -5,7 +5,7 @@ import org.apache.logging.log4j.Logger;
 
 import net.fabricmc.api.ModInitializer;
 
-import rsmm.fabric.command.argument.RSMMArgumentTypes;
+import rsmm.fabric.command.argument.RSMMCommandArgumentTypes;
 
 public class RedstoneMultimeterMod implements ModInitializer {
 	
@@ -13,7 +13,7 @@ public class RedstoneMultimeterMod implements ModInitializer {
 	
 	@Override
 	public void onInitialize() {
-		RSMMArgumentTypes.register();
+		RSMMCommandArgumentTypes.register();
 		
 		LOGGER.info("Redstone Multimeter has been initialized!");
 	}
