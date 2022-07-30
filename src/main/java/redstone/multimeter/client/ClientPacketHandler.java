0ae68bcfaaaced1de30d467bb6d1528997f678c6
Space Walker <spacewalkerrs@gmail.com>
2022-07-30 21:18:58 +0200

    update multiconnect compat

--- src/main/java/redstone/multimeter/client/ClientPacketHandler.java
+++ src/main/java/redstone/multimeter/client/ClientPacketHandler.java
@@ -1,16 +1,11 @@
 package redstone.multimeter.client;
 
-//import net.earthcomputer.multiconnect.api.ICustomPayloadEvent;
-//import net.earthcomputer.multiconnect.api.MultiConnectAPI;
-
-//import net.minecraft.client.MinecraftClient;
 import net.minecraft.network.Packet;
 import net.minecraft.network.PacketByteBuf;
 import net.minecraft.network.packet.c2s.play.CustomPayloadC2SPacket;
 import net.minecraft.util.Identifier;
 
 import redstone.multimeter.common.network.AbstractPacketHandler;
-//import redstone.multimeter.common.network.PacketManager;
 import redstone.multimeter.common.network.RSMMPacket;
 
 public class ClientPacketHandler extends AbstractPacketHandler {
@@ -19,29 +14,6 @@ public class ClientPacketHandler extends AbstractPacketHandler {
 	
 	public ClientPacketHandler(MultimeterClient client) {
 		this.client = client;
-		
-		/*MultiConnectAPI multiConnect = MultiConnectAPI.instance();
-		
-		multiConnect.addClientboundIdentifierCustomPayloadListener(event -> {
-			if (PacketManager.getPacketChannelId().equals(event.getChannel())) {
-				handleIncomingPayloadEvent(event);
-			}
-		});
-		multiConnect.addClientboundStringCustomPayloadListener(event -> {
-			if (PacketManager.getPacketChannelId().toString().equals(event.getChannel())) {
-				handleIncomingPayloadEvent(event);
-			}
-		});
-		multiConnect.addServerboundIdentifierCustomPayloadListener(event -> {
-			if (PacketManager.getPacketChannelId().equals(event.getChannel())) {
-				multiConnect.forceSendCustomPayload(event.getNetworkHandler(), event.getChannel(), event.getData());
-			}
-		});
-		multiConnect.addServerboundStringCustomPayloadListener(event -> {
-			if (PacketManager.getPacketChannelId().toString().equals(event.getChannel())) {
-				multiConnect.forceSendStringCustomPayload(event.getNetworkHandler(), event.getChannel(), event.getData());
-			}
-		});*/
 	}
 	
 	@Override
@@ -63,18 +35,4 @@ public class ClientPacketHandler extends AbstractPacketHandler {
 			buffer.release();
 		}
 	}
-	
-	/*private void handleIncomingPayloadEvent(ICustomPayloadEvent<?> event) {
-		MinecraftClient minecraftClient = client.getMinecraftClient();
-		
-		if (minecraftClient.isOnThread()) {
-			onPacketReceived(event.getData());
-		} else {
-			minecraftClient.execute(() -> {
-				if (event.getNetworkHandler().getConnection().isOpen()) {
-					onPacketReceived(event.getData());
-				}
-			});
-		}
-	}*/
 }
