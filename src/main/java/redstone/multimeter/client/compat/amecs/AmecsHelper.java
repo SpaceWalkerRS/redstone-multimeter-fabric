fb63eed3085d5c101279fdaa05c334b9bf04ad1d
Space Walker <spacewalkerrs@gmail.com>
2022-08-05 18:38:10 +0200

    fix crash

--- src/main/java/redstone/multimeter/client/compat/amecs/AmecsHelper.java
+++ src/main/java/redstone/multimeter/client/compat/amecs/AmecsHelper.java
@@ -9,10 +9,17 @@ import de.siphalor.amecs.api.KeyModifiers;
 import de.siphalor.amecs.impl.ModifierPrefixTextProvider.Variation;
 import de.siphalor.amecs.impl.duck.IKeyBinding;
 
+import net.fabricmc.loader.api.FabricLoader;
+
 import net.minecraft.client.options.KeyBinding;
+import net.minecraft.text.MutableText;
 import net.minecraft.text.Text;
 
+import redstone.multimeter.util.TextUtils;
+
 public class AmecsHelper {
+
+	private static boolean isAmecsApiLoaded = FabricLoader.getInstance().isModLoaded("amecsapi");
 
 	public static Collection<KeyModifier> getKeyModifiers(KeyBinding keybind) {
 		KeyModifiers keyModifiers = ((IKeyBinding)keybind).amecs$getKeyModifiers();
@@ -38,5 +45,17 @@ public class AmecsHelper {
 
 	public static Text getModifierName(KeyModifier modifier) {
 		return getModifierName(modifier, Variation.NORMAL);
+	}
+
+	public static MutableText addModifiers(MutableText text, KeyBinding keybind) {
+		if (isAmecsApiLoaded) {
+			for (KeyModifier modifier : getKeyModifiers(keybind)) {
+				text.
+					append(TextUtils.formatKey(getModifierName(modifier))).
+					append(" + ");
+			}
+		}
+		
+		return text;
 	}
 }
--- src/main/java/redstone/multimeter/util/TextUtils.java
+++ src/main/java/redstone/multimeter/util/TextUtils.java
@@ -5,8 +5,6 @@ import java.util.Collection;
 import java.util.LinkedList;
 import java.util.List;
 
-import de.siphalor.amecs.api.KeyModifier;
-
 import net.minecraft.client.font.TextRenderer;
 import net.minecraft.client.options.KeyBinding;
 import net.minecraft.client.util.InputUtil.Key;
@@ -151,15 +149,9 @@ public class TextUtils {
 			return text;
 		}
 		
+		AmecsHelper.addModifiers(text, keybind);
+		
 		Key boundKey = ((IKeyBinding)keybind).getBoundKeyRSMM();
-		Collection<KeyModifier> modifiers = AmecsHelper.getKeyModifiers(keybind);
-		
-		for (KeyModifier modifier : modifiers) {
-			text.
-				append(formatKey(AmecsHelper.getModifierName(modifier))).
-				append(" + ");
-		}
-		
 		text.append(formatKey(boundKey));
 		
 		return text;
