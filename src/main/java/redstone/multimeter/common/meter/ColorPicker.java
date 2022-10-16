6949a1ba76c6645321486f4a8d1cd9f5b96e5410
Space Walker <spacedoesrs@gmail.com>
2022-10-16 22:13:25 +0200

    add Color Picker option

---
+++ src/main/java/redstone/multimeter/common/meter/ColorPicker.java
@@ -0,0 +1,47 @@
+package redstone.multimeter.common.meter;
+
+import java.awt.Color;
+
+import redstone.multimeter.client.option.Cyclable;
+
+public enum ColorPicker implements Cyclable<ColorPicker> {
+
+	RANDOM("random") {
+
+		private int index;
+
+		@Override
+		public int next() {
+			float hue = ((index * 11) % 8 + (index / 8) / 2.0F) / 8.0F;
+			index = (index + 1) % 16;
+
+			return 0xFFFFFF & Color.HSBtoRGB(hue, 0.7F, 1.0F);
+		}
+	},
+	RAINBOW("rainbow") {
+
+		private int index;
+
+		@Override
+		public int next() {
+			float hue = index / 32.0F;
+			index = (index + 1) % 32;
+
+			return 0xFFFFFF & Color.HSBtoRGB(hue, 0.7F, 1.0F);
+		}
+	};
+
+	private ColorPicker(String name) {
+		this.name = name;
+	}
+
+	private final String name;
+
+	@Override
+	public String getName() {
+		return name;
+	}
+
+	public abstract int next();
+
+}
