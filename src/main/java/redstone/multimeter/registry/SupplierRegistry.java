6342a9020d23d4d686a3a28c91bd385b34da649f
Space Walker <spacedoesrs@gmail.com>
2022-10-17 16:12:34 +0200

    fix: some packets are sent to non-rsmm clients

--- src/main/java/redstone/multimeter/registry/SupplierRegistry.java
+++ src/main/java/redstone/multimeter/registry/SupplierRegistry.java
@@ -8,15 +8,15 @@ import net.minecraft.util.Identifier;
 
 import redstone.multimeter.RedstoneMultimeterMod;
 
-public class SupplierClazzRegistry<T> {
+public class SupplierRegistry<T> {
 	
 	private final Identifier id;
-	private final Map<Class<? extends T>, Identifier> clazzToId;
+	private final Map<Class<? extends T>, Identifier> typeToId;
 	private final Map<Identifier, Supplier<? extends T>> idToSupplier;
 	
-	public SupplierClazzRegistry(String name) {
+	public SupplierRegistry(String name) {
 		this.id = new Identifier(RedstoneMultimeterMod.NAMESPACE, name);
-		this.clazzToId = new HashMap<>();
+		this.typeToId = new HashMap<>();
 		this.idToSupplier = new HashMap<>();
 	}
 	
@@ -24,29 +24,28 @@ public class SupplierClazzRegistry<T> {
 		return id;
 	}
 	
-	@SuppressWarnings("unchecked")
-	public <P extends T> P get(Identifier id) {
+	public T get(Identifier id) {
 		Supplier<? extends T> objSupplier = idToSupplier.get(id);
-		return objSupplier == null ? null : (P)objSupplier.get();
+		return objSupplier == null ? null : objSupplier.get();
 	}
 	
-	public <P extends T> Identifier getId(P obj) {
-		return clazzToId.get(obj.getClass());
+	public Identifier getId(T obj) {
+		return typeToId.get(obj.getClass());
 	}
 	
-	public <P extends T> void register(String name, Class<P> clazz, Supplier<P> supplier) {
+	public <P extends T> void register(String name, Class<P> type, Supplier<P> supplier) {
 		String namespace = id.getNamespace();
 		String path = String.format("%s/%s", id.getPath(), name);
 		Identifier id = new Identifier(namespace, path);
 		
-		if (clazzToId.containsKey(clazz)) {
-			throw new IllegalStateException("Registry " + this.id + " already registered an entry with clazz " + clazz);
+		if (typeToId.containsKey(type)) {
+			throw new IllegalStateException("Registry " + this.id + " already registered an entry with type " + type);
 		}
 		if (idToSupplier.containsKey(id)) {
 			throw new IllegalStateException("Registry " + this.id + " already registered an entry with id " + id);
 		}
 		
-		clazzToId.put(clazz, id);
+		typeToId.put(type, id);
 		idToSupplier.put(id, supplier);
 	}
 }
