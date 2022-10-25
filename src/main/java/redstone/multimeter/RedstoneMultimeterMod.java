d9694111f065f630f67d3232f7b9b5f5352dc07b
Space Walker <spacedoesrs@gmail.com>
2022-10-25 13:49:08 +0200

    bump version to 1.11.0

--- src/main/java/redstone/multimeter/RedstoneMultimeterMod.java
+++ src/main/java/redstone/multimeter/RedstoneMultimeterMod.java
@@ -9,7 +9,7 @@ public class RedstoneMultimeterMod implements ModInitializer {
 	
 	public static final String MOD_ID = "rsmm_fabric";
 	public static final String MOD_NAME = "Redstone Multimeter";
-	public static final String MOD_VERSION = "1.10.0";
+	public static final String MOD_VERSION = "1.11.0";
 	public static final String NAMESPACE = "redstone_multimeter";
 	public static final String CONFIG_PATH = "config/" + NAMESPACE;
 	public static final Logger LOGGER = LogManager.getLogger(MOD_NAME);
