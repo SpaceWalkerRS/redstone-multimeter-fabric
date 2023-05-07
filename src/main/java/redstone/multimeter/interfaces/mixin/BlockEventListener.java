0055f160f15b216f22ecca43faf49c236165da6c
Space Walker <spacedoesrs@gmail.com>
2023-05-08 00:22:04 +0200

    update SubTick compat

---
+++ src/main/java/redstone/multimeter/interfaces/mixin/BlockEventListener.java
@@ -0,0 +1,11 @@
+package redstone.multimeter.interfaces.mixin;
+
+public interface BlockEventListener {
+
+	void rsmm$startBlockEvents();
+
+	void rsmm$nextBlockEvent();
+
+	void rsmm$endBlockEvents();
+
+}
