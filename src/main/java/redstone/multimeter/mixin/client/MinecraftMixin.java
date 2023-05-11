a7b5bd4a3d393eb0365fff6e855c2983234ed8c9
Space Walker <spacedoesrs@gmail.com>
2023-05-11 22:07:34 +0200

    fix reloadResourcePacks mixin target

--- src/main/java/redstone/multimeter/mixin/client/MinecraftMixin.java
+++ src/main/java/redstone/multimeter/mixin/client/MinecraftMixin.java
@@ -31,7 +31,7 @@ public class MinecraftMixin implements IMinecraft {
 	}
 
 	@Inject(
-		method = "reloadResourcePacks(Z)Ljava/util/concurrent/CompletableFuture;",
+		method = "reloadResourcePacks()Ljava/util/concurrent/CompletableFuture;",
 		at = @At(
 			value = "HEAD"
 		)
