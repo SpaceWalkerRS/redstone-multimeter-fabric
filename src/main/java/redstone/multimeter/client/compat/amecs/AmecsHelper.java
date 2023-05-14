7ef0d05c2ce1832458efe820f65cc682113a0a39
Space Walker <spacedoesrs@gmail.com>
2023-05-14 21:22:31 +0200

    update Amecs compatibility

--- src/main/java/redstone/multimeter/client/compat/amecs/AmecsHelper.java
+++ src/main/java/redstone/multimeter/client/compat/amecs/AmecsHelper.java
@@ -1,25 +1,28 @@
 package redstone.multimeter.client.compat.amecs;
-/*
+
 import java.util.Collection;
 import java.util.Collections;
 import java.util.LinkedList;
 
+import de.siphalor.amecs.api.KeyBindingUtils;
 import de.siphalor.amecs.api.KeyModifier;
 import de.siphalor.amecs.api.KeyModifiers;
 import de.siphalor.amecs.impl.ModifierPrefixTextProvider.Variation;
-import de.siphalor.amecs.impl.duck.IKeyBinding;
 
 import net.fabricmc.loader.api.FabricLoader;
-*/
+
 import net.minecraft.client.KeyMapping;
+import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.MutableComponent;
 
+import redstone.multimeter.util.TextUtils;
+
 public class AmecsHelper {
-/*
+
 	private static boolean isAmecsApiLoaded = FabricLoader.getInstance().isModLoaded("amecsapi");
 
-	public static Collection<KeyModifier> getKeyModifiers(KeyBinding keybind) {
-		KeyModifiers keyModifiers = ((IKeyBinding)keybind).amecs$getKeyModifiers();
+	public static Collection<KeyModifier> getKeyModifiers(KeyMapping keybind) {
+		KeyModifiers keyModifiers = KeyBindingUtils.getBoundModifiers(keybind);
 
 		if (keyModifiers.isUnset()) {
 			return Collections.emptyList();
@@ -36,23 +39,23 @@ public class AmecsHelper {
 		return modifiers;
 	}
 
-	public static Text getModifierName(KeyModifier modifier, Variation variation) {
-		return MutableText.of(variation.getTranslatableText(modifier.getTranslationKey()));
+	public static Component getModifierName(KeyModifier modifier, Variation variation) {
+		return MutableComponent.create(variation.getTranslatableText(modifier.getTranslationKey()));
 	}
 
-	public static Text getModifierName(KeyModifier modifier) {
+	public static Component getModifierName(KeyModifier modifier) {
 		return getModifierName(modifier, Variation.NORMAL);
 	}
-*/
+
 	public static MutableComponent addModifiers(MutableComponent text, KeyMapping keybind) {
-		/*if (isAmecsApiLoaded) {
+		if (isAmecsApiLoaded) {
 			for (KeyModifier modifier : getKeyModifiers(keybind)) {
 				text.
 					append(TextUtils.formatKey(getModifierName(modifier))).
 					append(" + ");
 			}
-		}*/
-		
+		}
+
 		return text;
 	}
 }
