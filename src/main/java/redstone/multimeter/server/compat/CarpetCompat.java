ff1e8b44895a861cfe4a3ac94ed7c5628fb269ad
Space Walker <spacedoesrs@gmail.com>
2024-01-14 12:59:25 +0100

    Loom 1.5, update carpet and subtick compat

--- src/main/java/redstone/multimeter/server/compat/CarpetCompat.java
+++ src/main/java/redstone/multimeter/server/compat/CarpetCompat.java
@@ -1,52 +1,30 @@
 package redstone.multimeter.server.compat;
 
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
-import java.util.function.Supplier;
+import carpet.fakes.MinecraftServerInterface;
+import carpet.helpers.ServerTickRateManager;
 
 import net.minecraft.server.MinecraftServer;
+
+import redstone.multimeter.RedstoneMultimeterMod;
 import redstone.multimeter.server.MultimeterServer;
 
 public class CarpetCompat {
 
 	private final MultimeterServer server;
 
-	private Supplier<Boolean> frozen = () -> false;
-
 	public CarpetCompat(MultimeterServer server) {
 		this.server = server;
 	}
 
-	public void init() {
-		Method MinecraftServer$getTickRateManager;
-		Class<?> ServerTickRateManager;
-		Method ServerTickRateManager$runsNormally;
+	public boolean isFrozen() {
+		if (RedstoneMultimeterMod.isCarpetPreset()) {
+			MinecraftServer server = this.server.getMinecraftServer();
+			ServerTickRateManager trm = ((MinecraftServerInterface)server).getTickRateManager();
 
-		Supplier<Boolean> frozen = () -> false;
+			return !trm.runsNormally();
 
-		try {
-			MinecraftServer$getTickRateManager = MinecraftServer.class.getMethod("getTickRateManager");
-			ServerTickRateManager = Class.forName("carpet.helpers.ServerTickRateManager");
-			ServerTickRateManager$runsNormally = ServerTickRateManager.getMethod("runsNormally");
-
-			Object trm = MinecraftServer$getTickRateManager.invoke(server.getMinecraftServer());
-
-			if (ServerTickRateManager$runsNormally.getReturnType() == boolean.class) {
-				frozen = () -> {
-					try {
-						return !(Boolean)ServerTickRateManager$runsNormally.invoke(trm);
-					} catch (IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
-						return false;
-					}
-				};
-			}
-		} catch (ClassNotFoundException | NoSuchMethodException | SecurityException | IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
 		}
 
-		this.frozen = frozen;
-	}
-
-	public boolean isFrozen() {
-		return frozen.get();
+		return false;
 	}
 }
--- src/main/java/redstone/multimeter/server/compat/SubTickCompat.java
+++ src/main/java/redstone/multimeter/server/compat/SubTickCompat.java
@@ -1,61 +1,23 @@
 package redstone.multimeter.server.compat;
 
-import java.lang.reflect.Field;
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
-import java.util.function.Supplier;
-
-import net.minecraft.server.level.ServerLevel;
-import net.minecraft.world.level.Level;
-
+import redstone.multimeter.RedstoneMultimeterMod;
 import redstone.multimeter.server.MultimeterServer;
+
+import subtick.TickHandler;
 
 public class SubTickCompat {
 
 	private final MultimeterServer server;
 
-	private Supplier<Boolean> frozen = () -> false;
-
 	public SubTickCompat(MultimeterServer server) {
 		this.server = server;
 	}
 
-	public void init() {
-		Class<?> SubTick;
-		Class<?> TickHandler;
-		Method SubTick$getTickHandler;
-		Field TickHandler$frozen;
-
-		Supplier<Boolean> frozen = () -> false;
-
-		try {
-			SubTick = Class.forName("subtick.SubTick");
-			SubTick$getTickHandler = SubTick.getMethod("getTickHandler", Level.class);
-			TickHandler = Class.forName("subtick.TickHandler");
-			TickHandler$frozen = TickHandler.getField("frozen");
-
-			if (TickHandler$frozen.getType() == boolean.class) {
-				for (ServerLevel level : server.getLevels()) {
-					Object tickHandler = SubTick$getTickHandler.invoke(null, level);
-
-					Supplier<Boolean> frozenChain = frozen;
-
-					frozen = () -> {
-						try {
-							return TickHandler$frozen.getBoolean(tickHandler);
-						} catch (IllegalArgumentException | IllegalAccessException e) {
-							return frozenChain.get();
-						}
-					};
-				}
-			}
-		} catch (ClassNotFoundException | NoSuchMethodException | NoSuchFieldException | SecurityException | IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
+	public boolean isFrozen() {
+		if (RedstoneMultimeterMod.isSubtickPreset()) {
+			return TickHandler.frozen();
 		}
 
-		this.frozen = frozen;
-	}
-
-	public boolean isFrozen() {
-		return frozen.get();
+		return false;
 	}
 }
