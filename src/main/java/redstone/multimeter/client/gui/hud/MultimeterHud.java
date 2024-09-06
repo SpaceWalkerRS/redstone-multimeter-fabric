d5353d4438242527541e7abc5f575f81ad601803
Space Walker <spacedoesrs@gmail.com>
2024-09-06 16:16:49 +0200

    update offset when options changed

--- src/main/java/redstone/multimeter/client/gui/hud/MultimeterHud.java
+++ src/main/java/redstone/multimeter/client/gui/hud/MultimeterHud.java
@@ -852,6 +852,10 @@ public class MultimeterHud extends AbstractParentElement {
 		meterGroupName.update();
 		playPauseButton.setVisible(onScreen || Options.HUD.PAUSE_INDICATOR.get());
 		onResized();
+
+		if (!paused) {
+			resetOffset();
+		}
 	}
 
 	public void updateTickMarkerColor() {
