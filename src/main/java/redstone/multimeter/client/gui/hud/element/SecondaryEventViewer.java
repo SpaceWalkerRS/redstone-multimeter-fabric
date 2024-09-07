fb858a20385b3d10625fbb9d5855dba10c0e8773
Space Walker <spacedoesrs@gmail.com>
2024-09-07 18:54:14 +0200

    hide subtick overview when previewing a meter group

--- src/main/java/redstone/multimeter/client/gui/hud/element/SecondaryEventViewer.java
+++ src/main/java/redstone/multimeter/client/gui/hud/element/SecondaryEventViewer.java
@@ -16,7 +16,7 @@ public class SecondaryEventViewer extends MeterEventViewer {
 
 	@Override
 	public void render(PoseStack poses, int mouseX, int mouseY) {
-		if (hud.isPaused() && getColumnCount() > 0) {
+		if (!hud.client.isPreviewing() && hud.isPaused() && getColumnCount() > 0) {
 			super.render(poses, mouseX, mouseY);
 		}
 	}
