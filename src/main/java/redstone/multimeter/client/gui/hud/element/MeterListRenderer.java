ee8e3b1b4584acc42d0f54abeaacfd41f73950a2
Space Walker <spacedoesrs@gmail.com>
2024-09-07 18:52:54 +0200

    highlight focussed meter in meter list

--- src/main/java/redstone/multimeter/client/gui/hud/element/MeterListRenderer.java
+++ src/main/java/redstone/multimeter/client/gui/hud/element/MeterListRenderer.java
@@ -205,6 +205,12 @@ public class MeterListRenderer extends AbstractElement {
 
 				drawHighlight(poses, highlightRow, true);
 			}
+		} else if (hud.isFocusMode()) {
+			int focussedRow = hud.getFocussedRow();
+
+			if (focussedRow != -1) {
+				drawHighlight(poses, focussedRow, true);
+			}
 		}
 	}
 
