45d6f692db44f627570de7ae2631fe0239953ca2
Space Walker <spacedoesrs@gmail.com>
2024-09-08 19:29:59 +0200

    fix block outline transforms if any meter name is rendered

--- src/main/java/redstone/multimeter/mixin/client/LevelRendererMixin.java
+++ src/main/java/redstone/multimeter/mixin/client/LevelRendererMixin.java
@@ -6,11 +6,12 @@ import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.At.Shift;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Slice;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import com.llamalad7.mixinextras.sugar.Local;
-import com.mojang.blaze3d.vertex.PoseStack;
 
+import com.mojang.blaze3d.vertex.PoseStack;
 import com.mojang.math.Matrix4f;
 
 import net.minecraft.client.Camera;
@@ -43,10 +44,17 @@ public class LevelRendererMixin {
 
 	@Inject(
 		method = "renderLevel",
+		slice = @Slice(
+			from = @At(
+				value = "INVOKE",
+				target = "Lnet/minecraft/client/renderer/debug/DebugRenderer;render(Lcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/client/renderer/MultiBufferSource$BufferSource;DDD)V"
+			)
+		),
 		at = @At(
 			value = "INVOKE",
+			ordinal = 0,
 			shift = Shift.AFTER,
-			target = "Lnet/minecraft/client/renderer/debug/DebugRenderer;render(Lcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/client/renderer/MultiBufferSource$BufferSource;DDD)V"
+			target = "Lcom/mojang/blaze3d/systems/RenderSystem;popMatrix()V"
 		)
 	)
 	private void renderMeterNames(PoseStack poses, float partialTick, long timeNanos, boolean renderBlockOutline, Camera camera, GameRenderer gameRenderer, LightTexture lightTexture, Matrix4f projectionPose, CallbackInfo ci, @Local MultiBufferSource.BufferSource bufferSource) {
