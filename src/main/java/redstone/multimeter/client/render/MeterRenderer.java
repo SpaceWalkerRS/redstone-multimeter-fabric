01d763562d85a9b004a27aa49d387539cf12f643
Space Walker <spacedoesrs@gmail.com>
2024-09-09 17:56:44 +0200

    add "when previewing" meter name mode

--- src/main/java/redstone/multimeter/client/render/MeterRenderer.java
+++ src/main/java/redstone/multimeter/client/render/MeterRenderer.java
@@ -50,7 +50,9 @@ public class MeterRenderer {
 	public void renderMeterNames(Matrix4f cameraPose, MultiBufferSource.BufferSource bufferSource) {
 		MeterNameMode mode = Options.RedstoneMultimeter.RENDER_METER_NAMES.get();
 
-		if (mode == MeterNameMode.ALWAYS || (mode == MeterNameMode.IN_FOCUS_MODE && client.getHud().isFocusMode())) {
+		if (mode == MeterNameMode.ALWAYS
+			|| (mode == MeterNameMode.WHEN_PREVIEWING && client.isPreviewing())
+			|| (mode == MeterNameMode.IN_FOCUS_MODE && client.getHud().isFocusMode() && !client.isPreviewing())) {
 			renderMeters(cameraPose, bufferSource, this::renderMeterName);
 		}
 	}
