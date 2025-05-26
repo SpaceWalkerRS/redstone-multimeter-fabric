8cbd4f845d2427709f9d2fc98138f6a9954586ee
Space Walker <spacedoesrs@gmail.com>
2025-05-26 17:00:28 +0200

    replace Redirect with Inject

--- src/main/java/redstone/multimeter/mixin/client/MinecraftMixin.java
+++ src/main/java/redstone/multimeter/mixin/client/MinecraftMixin.java
@@ -1,25 +1,32 @@
 package redstone.multimeter.mixin.client;
 
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.At.Shift;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.Slice;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import com.llamalad7.mixinextras.sugar.Local;
 
+import com.mojang.blaze3d.platform.InputConstants;
+
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.client.options.GameOptions;
 import net.minecraft.client.options.KeyBinding;
 import net.minecraft.client.world.ClientWorld;
 
 import redstone.multimeter.client.MultimeterClient;
+import redstone.multimeter.interfaces.mixin.IKeyBinding;
 import redstone.multimeter.interfaces.mixin.IMinecraft;
 
 @Mixin(Minecraft.class)
 public class MinecraftMixin implements IMinecraft {
+
+	@Shadow
+	private GameOptions options;
 
 	private MultimeterClient multimeterClient;
 
@@ -68,7 +75,7 @@ public class MinecraftMixin implements IMinecraft {
 		multimeterClient.getInputHandler().handleKeybinds();
 	}
 
-	@Redirect(
+	@Inject(
 		method = "handleKeyBindings",
 		slice = @Slice(
 			from = @At(
@@ -82,8 +89,19 @@ public class MinecraftMixin implements IMinecraft {
 			target = "Lnet/minecraft/client/options/KeyBinding;consumeClick()Z"
 		)
 	)
-	private boolean handleHotbarKeybinds(KeyBinding keybind, @Local int slot) {
-		return keybind.consumeClick() && !multimeterClient.getInputHandler().handleHotbarKeybinds(slot);
+	private void handleHotbarKeybinds(CallbackInfo ci, @Local int slot) {
+		KeyBinding keybind = options.horbarKeyBindings[slot];
+		InputConstants.Key key = ((IKeyBinding) keybind).rsmm$getKey();
+
+		while (keybind.consumeClick()) {
+			if (!multimeterClient.getInputHandler().handleHotbarKeybinds(slot)) {
+				// un-consume the click so the toolbar/
+				// select hotbar slot code can be handled
+				KeyBinding.click(key);
+
+				break;
+			}
+		}
 	}
 
 	@Inject(
