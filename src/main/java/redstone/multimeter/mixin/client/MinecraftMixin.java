4a9fb8242e776f364320a75657d97446d9a921e2
Space Walker <spacedoesrs@gmail.com>
2025-05-26 17:00:28 +0200

    replace Redirect with Inject

--- src/main/java/redstone/multimeter/mixin/client/MinecraftMixin.java
+++ src/main/java/redstone/multimeter/mixin/client/MinecraftMixin.java
@@ -1,17 +1,19 @@
 package redstone.multimeter.mixin.client;
 
 import org.lwjgl.input.Mouse;
+
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.At.Shift;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.Slice;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import com.llamalad7.mixinextras.sugar.Local;
 
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.options.GameOptions;
 import net.minecraft.client.options.KeyBinding;
 import net.minecraft.client.world.ClientWorld;
 
@@ -21,7 +23,16 @@ import redstone.multimeter.interfaces.mixin.IMinecraft;
 @Mixin(Minecraft.class)
 public class MinecraftMixin implements IMinecraft {
 
+	@Shadow
+	private GameOptions options;
+	@Shadow
+	private long sysTime;
+
 	private MultimeterClient multimeterClient;
+	private long savedSysTime;
+
+	@Shadow
+	private static long getTime() { return 0L; }
 
 	@Inject(
 		method = "init",
@@ -81,25 +92,39 @@ public class MinecraftMixin implements IMinecraft {
 		multimeterClient.getInputHandler().handleKeybinds();
 	}
 
-	@Redirect(
+	@Inject(
 		method = "handleMouseEvents",
 		at = @At(
 			value = "INVOKE",
-			remap = false,
-			target = "Lorg/lwjgl/input/Mouse;getEventDWheel()I"
+			target = "Lnet/minecraft/client/Minecraft;getTime()J"
 		)
 	)
-	private int handleScroll() {
-		int scrollY = Mouse.getEventDWheel();
+	private void handleScroll(CallbackInfo ci) {
+		savedSysTime = sysTime;
 
-		if (multimeterClient.getInputHandler().handleMouseScroll(0, scrollY)) {
-			return 0;
+		if (getTime() - sysTime <= 200) {
+			int scrollY = Mouse.getDWheel();
+
+			if (multimeterClient.getInputHandler().handleMouseScroll(0, scrollY)) {
+				// prevent vanilla handling of scroll event
+				sysTime = -Integer.MIN_VALUE;
+			}
 		}
-
-		return scrollY;
 	}
 
-	@Redirect(
+	@Inject(
+		method = "handleMouseEvents",
+		at = @At(
+			value = "FIELD",
+			shift = Shift.AFTER,
+			target = "Lnet/minecraft/client/Minecraft;sysTime:J"
+		)
+	)
+	private void restoreScroll(CallbackInfo ci) {
+		sysTime = savedSysTime;
+	}
+
+	@Inject(
 		method = "handleKeyBindings",
 		slice = @Slice(
 			from = @At(
@@ -113,8 +138,19 @@ public class MinecraftMixin implements IMinecraft {
 			target = "Lnet/minecraft/client/options/KeyBinding;consumeClick()Z"
 		)
 	)
-	private boolean handleHotbarKeybinds(KeyBinding keybind, @Local int slot) {
-		return keybind.consumeClick() && !multimeterClient.getInputHandler().handleHotbarKeybinds(slot);
+	private void handleHotbarKeybinds(CallbackInfo ci, @Local int slot) {
+		KeyBinding keybind = options.horbarKeyBindings[slot];
+		int key = keybind.getKeyCode();
+
+		while (keybind.consumeClick()) {
+			if (!multimeterClient.getInputHandler().handleHotbarKeybinds(slot)) {
+				// un-consume the click so the toolbar/
+				// select hotbar slot code can be handled
+				KeyBinding.click(key);
+
+				break;
+			}
+		}
 	}
 
 	@Inject(
