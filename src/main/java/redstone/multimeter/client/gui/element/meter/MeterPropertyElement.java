de96e1f06cca63865ae41319c356c980e5ae46d3
Space Walker <spacedoesrs@gmail.com>
2025-06-14 20:20:19 +0200

    update and improve text field auto-complete
    - show suggestions in a drop-down menu
    - this menu is navigable with arrow keys
    - the selected suggestion no longer renders inside the text field if the
      input matches in the middle, now only does when it matches the start

--- src/main/java/redstone/multimeter/client/gui/element/meter/MeterPropertyElement.java
+++ src/main/java/redstone/multimeter/client/gui/element/meter/MeterPropertyElement.java
@@ -23,8 +23,10 @@ import redstone.multimeter.client.gui.element.TextElement;
 import redstone.multimeter.client.gui.element.action.MousePress;
 import redstone.multimeter.client.gui.element.button.ButtonFactory;
 import redstone.multimeter.client.gui.element.button.IButton;
+import redstone.multimeter.client.gui.element.button.SuggestionsMenu;
 import redstone.multimeter.client.gui.element.button.SuggestionsProvider;
 import redstone.multimeter.client.gui.element.button.TextField;
+import redstone.multimeter.client.gui.element.button.TextFieldFactory;
 import redstone.multimeter.client.gui.element.button.TransparentButton;
 import redstone.multimeter.client.gui.element.button.TransparentToggleButton;
 import redstone.multimeter.common.DimPos;
@@ -64,7 +66,9 @@ public class MeterPropertyElement extends AbstractParentElement {
 	}
 
 	@Override
-	protected void onChangedX(int x) {
+	public void setX(int x) {
+		super.setX(x);
+
 		if (toggle == null) {
 			property.setX(x + 2);
 		} else {
@@ -76,7 +80,9 @@ public class MeterPropertyElement extends AbstractParentElement {
 	}
 
 	@Override
-	protected void onChangedY(int y) {
+	public void setY(int y) {
+		super.setY(y);
+
 		if (toggle != null) {
 			toggle.setY(y + IButton.DEFAULT_HEIGHT - (IButton.DEFAULT_HEIGHT + toggle.getHeight()) / 2);
 		}
@@ -93,6 +99,8 @@ public class MeterPropertyElement extends AbstractParentElement {
 	}
 
 	private void addControl(MeterControlElement control) {
+		control.addChildren();
+
 		controls.add(control);
 		controls.updateCoords();
 
@@ -107,12 +115,20 @@ public class MeterPropertyElement extends AbstractParentElement {
 		addControl(name, UnaryOperator.identity(), factory, tooltip);
 	}
 
+	public void addControl(String name, TextFieldFactory factory, SuggestionsProvider suggestions) {
+		addControl(name, UnaryOperator.identity(), factory, suggestions);
+	}
+
 	public void addControl(String name, UnaryOperator<Style> formatter, ButtonFactory factory) {
 		addControl(new MeterControlElement(name, formatter, factory));
 	}
 
 	public void addControl(String name, UnaryOperator<Style> formatter, ButtonFactory factory, Supplier<Tooltip> tooltip) {
 		addControl(new MeterControlElement(name, formatter, factory, tooltip));
+	}
+
+	public void addControl(String name, UnaryOperator<Style> formatter, TextFieldFactory factory, SuggestionsProvider suggestions) {
+		addControl(new TextFieldControlElement(name, formatter, factory, suggestions));
 	}
 
 	public void addCoordinateControl(Axis axis, Supplier<DimPos> getter, Consumer<DimPos> setter) {
@@ -150,9 +166,6 @@ public class MeterPropertyElement extends AbstractParentElement {
 			this.name = new TextElement(client, 0, 0, t -> t.setText(formatName(name, formatter)).setWithShadow(true), tooltip);
 			this.control = factory.create(client, buttonWidth, IButton.DEFAULT_HEIGHT);
 
-			addChild(this.name);
-			addChild(this.control);
-
 			setWidth(this.name.getWidth() + 4 + this.control.getWidth() + 10);
 			setHeight(IButton.DEFAULT_HEIGHT);
 		}
@@ -160,6 +173,19 @@ public class MeterPropertyElement extends AbstractParentElement {
 		@Override
 		public void setX(int x) {
 			super.setX(x + controls.getWidth() - getWidth());
+
+			name.setX(getX());
+			control.setX(getX() + name.getWidth() + 4);
+		}
+
+		@Override
+		public void setY(int y) {
+			super.setY(y);
+
+			int height = getHeight();
+
+			name.setY(y + height - (height + font.lineHeight) / 2);
+			control.setY(y);
 		}
 
 		@Override
@@ -168,24 +194,38 @@ public class MeterPropertyElement extends AbstractParentElement {
 			control.setActive(active);
 		}
 
-		@Override
-		protected void onChangedX(int x) {
-			name.setX(x);
-			control.setX(x + name.getWidth() + 4);
-		}
-
-		@Override
-		protected void onChangedY(int y) {
-			int height = getHeight();
-
-			name.setY(y + height - (height + font.lineHeight) / 2);
-			control.setY(y);
+		protected void addChildren() {
+			addChild(name);
+			addChild(control);
 		}
 
 		protected Component formatName(String name, UnaryOperator<Style> formatter) {
 			int width = controls.getWidth() - (4 + buttonWidth + 10);
 			MutableComponent text = new TextComponent(font.plainSubstrByWidth(name, width, true));
 			return active ? text.withStyle(formatter) : text.withStyle(ChatFormatting.GRAY);
+		}
+	}
+
+	private class TextFieldControlElement extends MeterControlElement {
+
+		private final SuggestionsMenu suggestions;
+
+		public TextFieldControlElement(String name, UnaryOperator<Style> formatter, TextFieldFactory factory, SuggestionsProvider suggestions) {
+			super(name, formatter, factory, () -> Tooltip.EMPTY);
+
+			this.suggestions = ((TextField) this.control).setSuggestions(suggestions);
+		}
+
+		@Override
+		public boolean isMouseOver(double mouseX, double mouseY) {
+			return super.isMouseOver(mouseX, mouseY) || suggestions.isMouseOver(mouseX, mouseY);
+		}
+
+		@Override
+		protected void addChildren() {
+			addChild(suggestions);
+
+			super.addChildren();
 		}
 	}
 
@@ -214,7 +254,7 @@ public class MeterPropertyElement extends AbstractParentElement {
 					int coord = axis.choose(p.getX(), p.getY(), p.getZ());
 
 					return String.valueOf(coord);
-				}, SuggestionsProvider.none());
+				});
 			});
 
 			int size = getHeight() / 2 - 1;
@@ -237,14 +277,11 @@ public class MeterPropertyElement extends AbstractParentElement {
 
 				return true;
 			});
-
-			addChild(this.increase);
-			addChild(this.decrease);
 		}
 
 		@Override
-		protected void onChangedX(int x) {
-			super.onChangedX(x);
+		public void setX(int x) {
+			super.setX(x);
 
 			x = control.getX() + control.getWidth();
 
@@ -253,13 +290,21 @@ public class MeterPropertyElement extends AbstractParentElement {
 		}
 
 		@Override
-		protected void onChangedY(int y) {
-			super.onChangedY(y);
+		public void setY(int y) {
+			super.setY(y);
 
 			y = control.getY() + 1;
 
 			increase.setY(y);
 			decrease.setY(y + increase.getHeight());
+		}
+
+		@Override
+		protected void addChildren() {
+			super.addChildren();
+
+			addChild(increase);
+			addChild(decrease);
 		}
 	}
 }
