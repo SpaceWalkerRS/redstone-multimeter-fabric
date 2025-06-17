72621a74743fb45e203795efd3b75f1d40394e82
Space Walker <spacedoesrs@gmail.com>
2025-06-17 17:22:29 +0200

    use smaller z offset for suggestions menu

--- src/main/java/redstone/multimeter/client/gui/element/button/SuggestionsMenu.java
+++ src/main/java/redstone/multimeter/client/gui/element/button/SuggestionsMenu.java
@@ -79,7 +79,7 @@ public class SuggestionsMenu extends AbstractElement {
 	public void render(PoseStack poses, int mouseX, int mouseY) {
 		if (isShowingSuggestions()) {
 			poses.pushPose();
-			poses.translate(0.0D, 0.0D, 1000.0D);
+			poses.translate(0.0D, 0.0D, 10.0D);
 
 			renderBackground(poses);
 			renderHighlights(poses);
