ad179c178684f4af417c9af831f40b3a2f3ecc26
Space Walker <spacedoesrs@gmail.com>
2025-06-30 16:59:28 +0200

    fix and improve timeline sizing and scaling

--- src/main/java/redstone/multimeter/client/gui/hud/MultimeterHud.java
+++ src/main/java/redstone/multimeter/client/gui/hud/MultimeterHud.java
@@ -131,8 +131,6 @@ public class MultimeterHud extends AbstractParentElement {
 
 		onScreen = false;
 
-		settings.rowHeight = Options.HUD.ROW_HEIGHT.get();
-
 		settings.forceFullOpacity = false;
 		settings.ignoreHiddenMeters = true;
 
@@ -321,7 +319,7 @@ public class MultimeterHud extends AbstractParentElement {
 				int color = ColorUtils.setAlpha(rgb, a);
 
 				if (!this.client.isPreviewing() && this.client.getMeterGroup().isDirty()) {
-					text = ChatFormatting.BOLD + text;
+					text = Formatting.BOLD + text;
 				}
 
 				t.setText(text);
@@ -407,7 +405,7 @@ public class MultimeterHud extends AbstractParentElement {
 		addChild(this.fastForwardButton);
 		addChild(this.printIndicator);
 
-		onOptionsChanged();
+		optionsChanged();
 		resetSize();
 	}
 
@@ -674,21 +672,6 @@ public class MultimeterHud extends AbstractParentElement {
 		updateTickMarkerCounter();
 	}
 
-	public boolean setTickMarkerColor(String rawColor) {
-		try {
-			int color = ColorUtils.fromRGBString(rawColor);
-			setTickMarkerColor(color);
-
-			return true;
-		} catch (NumberFormatException e) {
-			return false;
-		}
-	}
-
-	public void setTickMarkerColor(int color) {
-		settings.colorHighlightTickMarker = color;
-	}
-
 	private void updateTickMarkerCounter() {
 		tickMarkerCounter.update();
 
@@ -904,11 +887,8 @@ public class MultimeterHud extends AbstractParentElement {
 		}
 	}
 
-	public void onOptionsChanged() {
-		settings.columnWidth = Options.HUD.COLUMN_WIDTH.get();
-		settings.rowHeight = Options.HUD.ROW_HEIGHT.get();
-		settings.gridSize = Options.HUD.GRID_SIZE.get();
-		updateTickMarkerColor();
+	public void optionsChanged() {
+		settings.optionsChanged();
 
 		meterGroupSlot.update();
 		meterGroupName.update();
@@ -917,15 +897,6 @@ public class MultimeterHud extends AbstractParentElement {
 
 		if (!paused) {
 			resetOffset();
-		}
-	}
-
-	public void updateTickMarkerColor() {
-		String rawColor = Options.HUD.TICK_MARKER_COLOR.get();
-
-		if (!setTickMarkerColor(rawColor)) {
-			rawColor = Options.HUD.TICK_MARKER_COLOR.getDefault();
-			setTickMarkerColor(rawColor);
 		}
 	}
 
