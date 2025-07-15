29fea760ec67b3a0c0f24ea192fcbbd30383b039
Space Walker <spacedoesrs@gmail.com>
2025-07-15 19:03:13 +0200

    use GuiGraphics.innerBlit

--- src/main/java/redstone/multimeter/mixin/client/GuiGraphicsAccessor.java
+++ src/main/java/redstone/multimeter/mixin/client/GuiGraphicsAccessor.java
@@ -1,15 +1,23 @@
 package redstone.multimeter.mixin.client;
 
+import java.util.function.Function;
+
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.gen.Accessor;
+import org.spongepowered.asm.mixin.gen.Invoker;
 
 import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.renderer.MultiBufferSource;
+import net.minecraft.client.renderer.RenderType;
+import net.minecraft.resources.ResourceLocation;
 
 @Mixin(GuiGraphics.class)
 public interface GuiGraphicsAccessor {
 
 	@Accessor("bufferSource")
 	MultiBufferSource.BufferSource rsmm$getBufferSource();
+
+	@Invoker("innerBlit")
+	void rsmm$innerBlit(Function<ResourceLocation, RenderType> renderType, ResourceLocation location, int x0, int x1, int y0, int y1, float u0, float u1, float v0, float v1, int color);
 
 }
