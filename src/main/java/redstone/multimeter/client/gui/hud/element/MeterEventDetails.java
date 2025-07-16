1265d27632d6e7f63aa066eb92a0a62f4a17530b
Space Walker <spacedoesrs@gmail.com>
2025-07-16 11:14:19 +0200

    fix meter event details rendering

--- src/main/java/redstone/multimeter/client/gui/hud/element/MeterEventDetails.java
+++ src/main/java/redstone/multimeter/client/gui/hud/element/MeterEventDetails.java
@@ -32,62 +32,14 @@ public class MeterEventDetails extends AbstractElement {
 		EventLog event = getEvent();
 
 		if (event != null) {
-			renderer.tooltip(event.getTooltip(), -15, -15);
-			// TODO: remove
-//			renderEventDetails(renderer, event);
+			renderer.tooltip(event.getTooltip(), -15, 0);
 		}
 	}
 
 	@Override
 	public void mouseMove(double mouseX, double mouseY) {
 	}
-/*
-	private void renderEventDetails(EventLog event) {
-		Tooltip tooltip = event.getTooltip();
-		List<Component> lines = tooltip.getLines();
 
-		int x = 0;
-		int y = 0;
-
-		int width = tooltip.getWidth(hud.font) + 8;
-		int height = tooltip.getHeight(hud.font) + 8;
-
-		int backgroundColor = 0xF0100010;
-		int borderColor0 = hud.isOnScreen() ? 0x505000FF : 0xFF8000FF;
-		int borderColor1 = hud.isOnScreen() ? 0x5028007F : 0xFF5000C0;
-
-		GlStateManager.pushMatrix();
-		GlStateManager.translated(0, 0, 400);
-
-		// background
-		hud.renderer.renderRect(x    , y + 1         , width    , height - 2, backgroundColor); // center, left/right outer borders
-		hud.renderer.renderRect(x + 1, y             , width - 2, 1         , backgroundColor); // top outer border
-		hud.renderer.renderRect(x + 1, y + height - 1, width - 2, 1         , backgroundColor); // bottom outer border
-
-		// inner border
-		hud.renderer.renderGradient(x + 1        , y + 2         , 1        , height - 4, borderColor0, borderColor1); // left
-		hud.renderer.renderRect    (x + 1        , y + height - 2, width - 2, 1         , borderColor1);               // bottom
-		hud.renderer.renderGradient(x + width - 2, y + 2         , 1        , height - 4, borderColor0, borderColor1); // right
-		hud.renderer.renderRect    (x + 1        , y + 1         , width - 2, 1         , borderColor0);               // top
-
-		boolean leftToRight = Options.HUD.DIRECTIONALITY_X.get() == Directionality.X.LEFT_TO_RIGHT;
-		boolean topToBottom = Options.HUD.DIRECTIONALITY_Y.get() == Directionality.Y.TOP_TO_BOTTOM;
-
-		int textX = x + 4;
-		int textY = y + 4;
-
-		for (int index = 0; index < lines.size(); index++) {
-			Component line = lines.get(index);
-
-			int lineX = textX + (leftToRight ? 0 : (width - 8) - hud.font.width(line.getColoredString()));
-			int lineY = textY + (topToBottom ? 1 : -1) * (index + (topToBottom ? 0 : 1)) * (hud.font.lineHeight + 1) + (topToBottom ? 0 : (height - 4));
-
-			hud.renderer.renderText(line, lineX, lineY, 0xFFFFFFFF);
-		}
-
-		GlStateManager.popMatrix();
-	}
-*/
 	@Override
 	public boolean mouseDrag(double mouseX, double mouseY, int button, double deltaX, double deltaY) {
 		return false;
