4b6a5cf02a04d5edd71c76d1e716cc80bb1ccdf3
Space Walker <spacedoesrs@gmail.com>
2025-07-16 15:55:44 +0200

    revert to old background texture

--- src/main/java/redstone/multimeter/client/gui/texture/Textures.java
+++ src/main/java/redstone/multimeter/client/gui/texture/Textures.java
@@ -4,7 +4,7 @@ import net.minecraft.resources.ResourceLocation;
 
 public class Textures {
 
-	public static final Texture OPTIONS_BACKGROUND    = new Texture(new ResourceLocation("textures/gui/inworld_menu_list_background.png"), 16, 16);
+	public static final Texture OPTIONS_BACKGROUND    = new Texture(new ResourceLocation("textures/gui/menu_background.png"), 16, 16);
 	public static final Texture TUTORIAL_TOAST        = new Texture(new ResourceLocation("textures/gui/sprites/toast/tutorial.png"), 160, 32);
 	public static final Texture BASIC_BUTTON_INACTIVE = new Texture(new ResourceLocation("textures/gui/sprites/widget/button_disabled.png"), 200, 20);
 	public static final Texture BASIC_BUTTON          = new Texture(new ResourceLocation("textures/gui/sprites/widget/button.png"), 200, 20);
