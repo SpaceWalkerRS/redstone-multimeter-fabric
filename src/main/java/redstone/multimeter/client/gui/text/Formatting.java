eafafc74c76be4791887eab4a53ef05f15b2f90e
Space Walker <spacedoesrs@gmail.com>
2025-07-20 12:37:28 +0200

    add resolving Text from formatted string

--- src/main/java/redstone/multimeter/client/gui/text/Formatting.java
+++ src/main/java/redstone/multimeter/client/gui/text/Formatting.java
@@ -65,12 +65,16 @@ public enum Formatting {
 	}
 
 	public static Formatting resolve(net.minecraft.text.Formatting formatting) {
+		return byCode(formatting.toString().charAt(1));
+	}
+
+	public static Formatting byCode(char code) {
 		for (Formatting f : Formatting.values()) {
-			if (f.resolved == formatting) {
+			if (f.code == code) {
 				return f;
 			}
 		}
 
-		throw new IllegalStateException("unknown chat formatting code " + formatting.toString().charAt(1));
+		throw new IllegalStateException("unknown chat formatting code " + code);
 	}
 }
