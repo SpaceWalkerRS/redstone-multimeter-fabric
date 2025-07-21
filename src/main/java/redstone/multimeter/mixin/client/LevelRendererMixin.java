30c6e5d32e140bf3a584dad2bca0d77978ec73ab
Space Walker <spacedoesrs@gmail.com>
2025-07-21 13:15:19 +0200

    fix meter name tags rendering behind translucent blocks

--- src/main/java/redstone/multimeter/mixin/client/LevelRendererMixin.java
+++ src/main/java/redstone/multimeter/mixin/client/LevelRendererMixin.java
@@ -6,56 +6,43 @@ import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.At.Shift;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.Slice;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-import com.llamalad7.mixinextras.sugar.Local;
 import com.mojang.blaze3d.vertex.PoseStack;
 import com.mojang.math.Matrix4f;
 
 import net.minecraft.client.Camera;
-import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.GameRenderer;
 import net.minecraft.client.renderer.LevelRenderer;
 import net.minecraft.client.renderer.LightTexture;
-import net.minecraft.client.renderer.MultiBufferSource;
+import net.minecraft.client.renderer.MultiBufferSource.BufferSource;
 import net.minecraft.client.renderer.RenderBuffers;
 
-import redstone.multimeter.interfaces.mixin.IMinecraft;
+import redstone.multimeter.client.MultimeterClient;
+import redstone.multimeter.client.render.MeterRenderer;
 
 @Mixin(LevelRenderer.class)
 public class LevelRendererMixin {
 
-	@Shadow @Final private Minecraft minecraft;
 	@Shadow @Final private RenderBuffers renderBuffers;
 
 	@Inject(
 		method = "renderLevel",
 		at = @At(
 			value = "INVOKE",
-			shift = Shift.BEFORE,
-			target = "Lnet/minecraft/client/renderer/FogRenderer;setupNoFog()V"
+			shift = Shift.AFTER,
+			target = "Lnet/minecraft/client/renderer/LevelRenderer;renderWorldBorder(Lnet/minecraft/client/Camera;)V"
 		)
 	)
-	private void renderMeterHighlights(PoseStack poses, float partialTick, long timeNanos, boolean renderBlockOutline, Camera camera, GameRenderer gameRenderer, LightTexture lightTexture, Matrix4f projectionPose, CallbackInfo ci) {
-		((IMinecraft)minecraft).getMultimeterClient().getMeterRenderer().renderMeters(poses);
-	}
+	private void renderMeters(PoseStack poses, float partialTick, long maxRenderTime, boolean renderBlockOutline, Camera camera, GameRenderer gameRenderer, LightTexture lightTexture, Matrix4f projectionPose, CallbackInfo ci) {
+		BufferSource bufferSource = renderBuffers.bufferSource();
+		MeterRenderer renderer = MultimeterClient.INSTANCE.getMeterRenderer();
 
-	@Inject(
-		method = "renderLevel",
-		slice = @Slice(
-			from = @At(
-				value = "INVOKE",
-				target = "Lnet/minecraft/client/renderer/debug/DebugRenderer;render(Lcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/client/renderer/MultiBufferSource$BufferSource;DDD)V"
-			)
-		),
-		at = @At(
-			value = "INVOKE",
-			ordinal = 0,
-			target = "Lcom/mojang/blaze3d/systems/RenderSystem;applyModelViewMatrix()V"
-		)
-	)
-	private void renderMeterNames(PoseStack poses, float partialTick, long timeNanos, boolean renderBlockOutline, Camera camera, GameRenderer gameRenderer, LightTexture lightTexture, Matrix4f projectionPose, CallbackInfo ci, @Local MultiBufferSource.BufferSource bufferSource) {
-		((IMinecraft)minecraft).getMultimeterClient().getMeterRenderer().renderMeterNames(poses, bufferSource);
+		poses = new PoseStack();
+
+		renderer.renderMeters(poses, bufferSource);
+		renderer.renderMeterNameTags(poses, bufferSource);
+
+		bufferSource.endBatch();
 	}
 }
