b93ecfef9ac75313a92efadb5aeb447f8e0b1762
Space Walker <spacedoesrs@gmail.com>
2025-07-23 13:25:20 +0200

    fix parsing legacy enum values

--- src/main/java/redstone/multimeter/client/Keybinds.java
+++ src/main/java/redstone/multimeter/client/Keybinds.java
@@ -27,6 +27,7 @@ public class Keybinds {
 
 	private static final Set<String> CATEGORIES = new LinkedHashSet<>();
 	private static final Map<String, KeyBinding> KEYBINDS = new LinkedHashMap<>();
+	// used for parsing keybinds from before RSMM 1.16
 	private static final Map<String, KeyBinding> LEGACY_KEYBINDS = new HashMap<>();
 
 	public static final KeyBinding[] TOGGLE_EVENT_TYPES = new KeyBinding[EventType.ALL.length];
