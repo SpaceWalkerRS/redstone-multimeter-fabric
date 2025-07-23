bd471735c547c9b1c608feb032096ca9ea37d9a4
Space Walker <spacedoesrs@gmail.com>
2025-07-23 13:25:20 +0200

    fix parsing legacy enum values

--- src/main/java/redstone/multimeter/client/gui/hud/Orientation.java
+++ src/main/java/redstone/multimeter/client/gui/hud/Orientation.java
@@ -6,35 +6,51 @@ public class Orientation {
 
 	public static enum X implements Cyclable<X> {
 
-		LEFT_TO_RIGHT("leftToRight"),
-		RIGHT_TO_LEFT("rightToLeft");
+		LEFT_TO_RIGHT("leftToRight", "LEFT_TO_RIGHT"),
+		RIGHT_TO_LEFT("rightToLeft", "RIGHT_TO_LEFT");
 
 		private final String key;
+		// used for parsing values from before RSMM 1.16
+		private final String legacyKey;
 
-		private X(String key) {
+		private X(String key, String legacyKey) {
 			this.key = key;
+			this.legacyKey = legacyKey;
 		}
 
 		@Override
 		public String key() {
-			return key;
+			return this.key;
+		}
+
+		@Override
+		public String legacyKey() {
+			return this.legacyKey;
 		}
 	}
 
 	public static enum Y implements Cyclable<Y> {
 
-		TOP_TO_BOTTOM("topToBottom"),
-		BOTTOM_TO_TOP("bottomToTop");
+		TOP_TO_BOTTOM("topToBottom", "TOP_TO_BOTTOM"),
+		BOTTOM_TO_TOP("bottomToTop", "BOTTOM_TO_TOP");
 
 		private final String key;
+		// used for parsing values from before RSMM 1.16
+		private final String legacyKey;
 
-		private Y(String key) {
+		private Y(String key, String legacyKey) {
 			this.key = key;
+			this.legacyKey = legacyKey;
 		}
 
 		@Override
 		public String key() {
-			return key;
+			return this.key;
+		}
+
+		@Override
+		public String legacyKey() {
+			return this.legacyKey;
 		}
 	}
 }
