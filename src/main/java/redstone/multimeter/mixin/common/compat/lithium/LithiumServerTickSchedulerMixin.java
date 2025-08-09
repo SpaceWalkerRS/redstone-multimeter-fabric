55986afeb336aa2973db7b8d096d1d583b9592eb
Space Walker <spacedoesrs@gmail.com>
2025-08-09 17:26:21 +0200

    fix tick scheduling not captured with lithium installed
    fixes #40

---
+++ src/main/java/redstone/multimeter/mixin/common/compat/lithium/LithiumServerTickSchedulerMixin.java
@@ -0,0 +1,50 @@
+package redstone.multimeter.mixin.common.compat.lithium;
+
+import java.util.Map;
+
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Pseudo;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import net.minecraft.core.BlockPos;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.level.TickNextTickData;
+import net.minecraft.world.level.TickPriority;
+
+import redstone.multimeter.interfaces.mixin.IServerLevel;
+
+@Pseudo
+@Mixin(targets = "me/jellysquid/mods/lithium/common/world/scheduler/LithiumServerTickScheduler")
+public class LithiumServerTickSchedulerMixin {
+
+	@Shadow @Final private ServerLevel world;
+	@Shadow @Final private Map<TickNextTickData<?>, ?> scheduledTicks;
+
+	private int rsmm$size;
+
+	@Inject(
+		method = "scheduleTick",
+		at = @At(
+			value = "HEAD"
+		)
+	)
+	private void onSchedule(BlockPos pos, Object type, int delay, TickPriority priority, CallbackInfo ci) {
+		rsmm$size = scheduledTicks.size();
+	}
+
+	@Inject(
+		method = "scheduleTick",
+		at = @At(
+			value = "TAIL"
+		)
+	)
+	private void logSchedule(BlockPos pos, Object type, int delay, TickPriority priority, CallbackInfo ci) {
+		if (rsmm$size < scheduledTicks.size()) {
+			((IServerLevel)world).getMultimeter().logScheduledTick(world, pos, priority, true);
+		}
+	}
+}
