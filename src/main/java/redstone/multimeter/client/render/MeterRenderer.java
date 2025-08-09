0fd8a1e751f744a1d5720a6281a069f55494bed0
Space Walker <spacedoesrs@gmail.com>
2025-08-09 20:20:47 +0200

    add meterHighlights and meterHighlightsRange settings

--- src/main/java/redstone/multimeter/client/render/MeterRenderer.java
+++ src/main/java/redstone/multimeter/client/render/MeterRenderer.java
@@ -1,5 +1,7 @@
 package redstone.multimeter.client.render;
 
+import java.util.function.Predicate;
+
 import org.lwjgl.opengl.GL11;
 
 import com.mojang.blaze3d.vertex.BufferBuilder;
@@ -26,51 +28,48 @@ public class MeterRenderer {
 		this.minecraft = this.client.getMinecraft();
 	}
 
-	public void renderMeters(float tickDelta) {
+	public void renderMeterHighlights(float tickDelta) {
 		GL11.glDisable(GL11.GL_TEXTURE_2D);
 		GL11.glDisable(GL11.GL_CULL_FACE);
 
-		renderMeters(this::renderMeterHighlight, tickDelta);
+		renderMeters(this::shouldRenderHighlight, this::renderHighlight, tickDelta);
 	}
 
 	public void renderMeterNameTags(float tickDelta) {
 		GL11.glDisable(GL11.GL_DEPTH_TEST);
 		GL11.glEnable(GL11.GL_TEXTURE_2D);
 
-		MeterNameMode mode = Options.RedstoneMultimeter.RENDER_METER_NAMES.get();
-
-		if (mode == MeterNameMode.ALWAYS
-			|| (mode == MeterNameMode.WHEN_PREVIEWING && client.isPreviewing())
-			|| (mode == MeterNameMode.IN_FOCUS_MODE && client.getHud().isFocusMode() && !client.isPreviewing())) {
-			renderMeters(this::renderMeterNameTag, tickDelta);
-		}
+		renderMeters(this::shouldRenderNameTag, this::renderNameTag, tickDelta);
 
 		GL11.glEnable(GL11.GL_DEPTH_TEST);
 	}
 
-	private void renderMeters(MeterPartRenderer renderer, float tickDelta) {
-		if (client.isPreviewing() || !client.getHud().isFocusMode()) {
-			ClientMeterGroup meterGroup = client.isPreviewing() ? client.getMeterGroupPreview() : client.getMeterGroup();
+	private void renderMeters(Predicate<Meter> predicate, MeterPartRenderer renderer, float tickDelta) {
+		ClientMeterGroup meterGroup = client.isPreviewing() ? client.getMeterGroupPreview() : client.getMeterGroup();
 
-			for (Meter meter : meterGroup.getMeters()) {
-				if (meter.isIn(minecraft.world)) {
-					renderer.render(meter, tickDelta);
-				}
-			}
-		} else {
-			Meter focussed = client.getHud().getFocussedMeter();
-
-			if (focussed != null) {
-				if (focussed.isIn(minecraft.world)) {
-					renderer.render(focussed, tickDelta);
-				}
+		for (Meter meter : meterGroup.getMeters()) {
+			if (meter.isIn(minecraft.world) && predicate.test(meter)) {
+				renderer.render(meter, tickDelta);
 			}
 		}
 	}
 
-	private void renderMeterHighlight(Meter meter, float tickDelta) {
-		BufferBuilder bufferBuilder = BufferBuilder.INSTANCE;
+	private boolean shouldRenderHighlight(Meter meter) {
+		switch (Options.RedstoneMultimeter.RENDER_METERS.get()) {
+		case ALWAYS:
+			return true;
+		case IN_FOCUS:
+			return !client.isPreviewing() && client.getHud().isFocusMode() && client.getHud().getFocussedMeter() == meter;
+		case IN_FOCUS_MODE:
+			return !client.isPreviewing() && client.getHud().isFocusMode();
+		case NEVER:
+			return false;
+		default:
+			throw new IllegalStateException("unknown meter highlight mode " + Options.RedstoneMultimeter.RENDER_METERS.getAsString());
+		}
+	}
 
+	private void renderHighlight(Meter meter, float tickDelta) {
 		DimPos pos = meter.getPos();
 		int color = meter.getColor();
 		boolean movable = meter.isMovable();
@@ -83,24 +82,53 @@ public class MeterRenderer {
 		double dx = pos.getX() - cameraX;
 		double dy = pos.getY() - cameraY;
 		double dz = pos.getZ() - cameraZ;
+		double distanceSquared = dx * dx + dy * dy + dz * dz;
 
-		GL11.glPushMatrix();;
-		GL11.glTranslated(dx, dy, dz);
+		int range = Options.RedstoneMultimeter.METER_RANGE.get();
+		double rangeSquared = range * range;
 
-		float r = ColorUtils.getRed(color) / (float) 0xFF;
-		float g = ColorUtils.getGreen(color) / (float) 0xFF;
-		float b = ColorUtils.getBlue(color) / (float) 0xFF;
+		if (range < 0 || distanceSquared < rangeSquared) {
+			BufferBuilder tesselator = BufferBuilder.INSTANCE;
 
-		renderMeterHighlight(bufferBuilder, r, g, b, 0.5F);
+			int dim = getHighlightDimmingFactor(meter);
 
-		if (movable) {
-			renderMeterOutline(bufferBuilder, r, g, b, 1.0F);
+			float r = ColorUtils.getRed(color) / (float) 0xFF;
+			float g = ColorUtils.getGreen(color) / (float) 0xFF;
+			float b = ColorUtils.getBlue(color) / (float) 0xFF;
+
+			GL11.glPushMatrix();;
+			GL11.glTranslated(dx, dy, dz);
+
+			renderMeterHighlight(tesselator, r, g, b, 0.5F / dim);
+
+			if (movable) {
+				renderMeterOutline(tesselator, r, g, b, 1.0F / dim);
+			}
+
+			GL11.glPopMatrix();
 		}
-
-		GL11.glPopMatrix();
 	}
 
-	private void renderMeterNameTag(Meter meter, float tickDelta) {
+	private int getHighlightDimmingFactor(Meter meter) {
+		return shouldDimMeter(meter) ? 3 : 1;
+	}
+
+	private boolean shouldRenderNameTag(Meter meter) {
+		switch (Options.RedstoneMultimeter.RENDER_METER_NAMES.get()) {
+		case ALWAYS:
+			return true;
+		case IN_FOCUS_MODE:
+			return !client.isPreviewing() && client.getHud().isFocusMode();
+		case WHEN_PREVIEWING:
+			return client.isPreviewing();
+		case NEVER:
+			return false;
+		default:
+			throw new IllegalStateException("unknown meter name tag mode " + Options.RedstoneMultimeter.RENDER_METER_NAMES.getAsString());
+		}
+	}
+
+	private void renderNameTag(Meter meter, float tickDelta) {
 		String name = meter.getName();
 		DimPos pos = meter.getPos();
 
@@ -118,85 +146,96 @@ public class MeterRenderer {
 		double rangeSquared = range * range;
 
 		if (distanceSquared < rangeSquared) {
-			renderNameTag(minecraft.textRenderer, name, dx + 0.5D, dy + 0.75D, dz + 0.5D);
+			int dim = getNameTagDimmingFactor(meter);
+			int color = ColorUtils.setAlpha(0xFFFFFF, 0xFF / dim);
+
+			renderNameTag(minecraft.textRenderer, name, dx + 0.5D, dy + 0.75D, dz + 0.5D, color);
 		}
 	}
 
-	private void renderMeterHighlight(BufferBuilder bufferBuilder, float r, float g, float b, float a) {
-		bufferBuilder.start(GL11.GL_QUADS);
-		drawBox(bufferBuilder, r, g, b, a, false);
-		bufferBuilder.end();
+	private int getNameTagDimmingFactor(Meter meter) {
+		return shouldDimMeter(meter) ? 2 : 1;
 	}
 
-	private void renderMeterOutline(BufferBuilder bufferBuilder, float r, float g, float b, float a) {
-		bufferBuilder.start(GL11.GL_LINES);
-		drawBox(bufferBuilder, r, g, b, a, true);
-		bufferBuilder.end();
+	private boolean shouldDimMeter(Meter meter) {
+		return !client.isPreviewing() && client.getHud().isFocusMode() && client.getHud().getFocussedMeter() != meter;
 	}
 
-	private void drawBox(BufferBuilder buffer, float r, float g, float b, float a, boolean outline) {
+	private void renderMeterHighlight(BufferBuilder tesselator, float r, float g, float b, float a) {
+		tesselator.start(GL11.GL_QUADS);
+		drawBox(tesselator, r, g, b, a, false);
+		tesselator.end();
+	}
+
+	private void renderMeterOutline(BufferBuilder tesselator, float r, float g, float b, float a) {
+		tesselator.start(GL11.GL_LINES);
+		drawBox(tesselator, r, g, b, a, true);
+		tesselator.end();
+	}
+
+	private void drawBox(BufferBuilder tesselator, float r, float g, float b, float a, boolean outline) {
 		// The box is slightly larger than 1x1 to prevent z-fighting
 		float c0 = -0.002F;
 		float c1 = 1.002F;
 
-		buffer.color(r, g, b, a);
+		tesselator.color(r, g, b, a);
 
 		// West face
-		buffer.vertex(c0, c0, c0);
-		buffer.vertex(c0, c0, c1);
-		buffer.vertex(c0, c1, c1);
-		buffer.vertex(c0, c1, c0);
+		tesselator.vertex(c0, c0, c0);
+		tesselator.vertex(c0, c0, c1);
+		tesselator.vertex(c0, c1, c1);
+		tesselator.vertex(c0, c1, c0);
 		if (outline) {
-			buffer.vertex(c0, c0, c0);
+			tesselator.vertex(c0, c0, c0);
 		}
 
 		// East face
-		buffer.vertex(c1, c0, c0);
-		buffer.vertex(c1, c1, c0);
-		buffer.vertex(c1, c1, c1);
-		buffer.vertex(c1, c0, c1);
+		tesselator.vertex(c1, c0, c0);
+		tesselator.vertex(c1, c1, c0);
+		tesselator.vertex(c1, c1, c1);
+		tesselator.vertex(c1, c0, c1);
 		if (outline) {
-			buffer.vertex(c1, c0, c0);
+			tesselator.vertex(c1, c0, c0);
 		}
 
 		// North face
-		buffer.vertex(c0, c0, c0);
-		buffer.vertex(c0, c1, c0);
-		buffer.vertex(c1, c1, c0);
-		buffer.vertex(c1, c0, c0);
+		tesselator.vertex(c0, c0, c0);
+		tesselator.vertex(c0, c1, c0);
+		tesselator.vertex(c1, c1, c0);
+		tesselator.vertex(c1, c0, c0);
 		if (outline) {
-			buffer.vertex(c0, c0, c0);
+			tesselator.vertex(c0, c0, c0);
 		}
 
 		// South face
-		buffer.vertex(c0, c0, c1);
-		buffer.vertex(c1, c0, c1);
-		buffer.vertex(c1, c1, c1);
-		buffer.vertex(c0, c1, c1);
+		tesselator.vertex(c0, c0, c1);
+		tesselator.vertex(c1, c0, c1);
+		tesselator.vertex(c1, c1, c1);
+		tesselator.vertex(c0, c1, c1);
 		if (outline) {
-			buffer.vertex(c0, c0, c1);
+			tesselator.vertex(c0, c0, c1);
 		}
 
 		// Bottom face
-		buffer.vertex(c0, c0, c0);
-		buffer.vertex(c1, c0, c0);
-		buffer.vertex(c1, c0, c1);
-		buffer.vertex(c0, c0, c1);
+		tesselator.vertex(c0, c0, c0);
+		tesselator.vertex(c1, c0, c0);
+		tesselator.vertex(c1, c0, c1);
+		tesselator.vertex(c0, c0, c1);
 		if (outline) {
-			buffer.vertex(c0, c0, c0);
+			tesselator.vertex(c0, c0, c0);
 		}
 
 		// Top face
-		buffer.vertex(c0, c1, c0);
-		buffer.vertex(c0, c1, c1);
-		buffer.vertex(c1, c1, c1);
-		buffer.vertex(c1, c1, c0);
+		tesselator.vertex(c0, c1, c0);
+		tesselator.vertex(c0, c1, c1);
+		tesselator.vertex(c1, c1, c1);
+		tesselator.vertex(c1, c1, c0);
 		if (outline) {
-			buffer.vertex(c0, c1, c0);
+			tesselator.vertex(c0, c1, c0);
 		}
 	}
 
-	private void renderNameTag(TextRenderer textRenderer, String name, double dx, double dy, double dz) {
+	private void renderNameTag(TextRenderer textRenderer, String name, double dx, double dy, double dz, int color) {
 		EntityRenderDispatcher entityRenderDispatcher = EntityRenderDispatcher.INSTANCE;
 
 		float yaw = entityRenderDispatcher.cameraYaw;
@@ -209,7 +248,7 @@ public class MeterRenderer {
 		GL11.glRotatef(pitch, 1.0F, 0.0F, 0.0F);
 		GL11.glScalef(-0.025F, -0.025F, 0.025F);
 
-		textRenderer.draw(name, -textRenderer.getWidth(name) / 2, 0, 0xFFFFFFFF);
+		textRenderer.draw(name, -textRenderer.getWidth(name) / 2, 0, color);
 
 		GL11.glPopMatrix();
 	}
