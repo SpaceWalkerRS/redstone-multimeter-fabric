d4e4865407612482e5db60f6f89b0cc9311c2bb3
Space Walker <spacedoesrs@gmail.com>
2025-09-14 12:24:31 +0200

    re-introduce Amecs compat

--- src/main/java/redstone/multimeter/client/compat/amecs/AmecsHelper.java
+++ src/main/java/redstone/multimeter/client/compat/amecs/AmecsHelper.java
@@ -1,12 +1,18 @@
 package redstone.multimeter.client.compat.amecs;
 
+import java.util.Collection;
+import java.util.Collections;
+import java.util.LinkedList;
+
+import de.siphalor.amecs.api.KeyModifier;
 import de.siphalor.amecs.api.KeyModifiers;
+import de.siphalor.amecs.impl.ModifierPrefixTextProvider.Variation;
 import de.siphalor.amecs.impl.duck.IKeyBinding;
 
 import net.fabricmc.loader.api.FabricLoader;
+
 import net.minecraft.client.KeyMapping;
 
-import redstone.multimeter.client.gui.text.Formatting;
 import redstone.multimeter.client.gui.text.Text;
 import redstone.multimeter.client.gui.text.Texts;
 
@@ -14,16 +20,43 @@ public class AmecsHelper {
 
 	private static boolean isAmecsApiLoaded = FabricLoader.getInstance().isModLoaded("amecsapi");
 
+	public static Collection<KeyModifier> getKeyModifiers(KeyMapping keybind) {
+		KeyModifiers keyModifiers = ((IKeyBinding)keybind).amecs$getKeyModifiers();
+
+		if (keyModifiers.isUnset()) {
+			return Collections.emptyList();
+		}
+
+		Collection<KeyModifier> modifiers = new LinkedList<>();
+
+		for (KeyModifier modifier : KeyModifier.VALUES) {
+			if (modifier != KeyModifier.NONE && keyModifiers.get(modifier)) {
+				modifiers.add(modifier);
+			}
+		}
+
+		return modifiers;
+	}
+
+	public static Text getModifierName(KeyModifier modifier, Variation variation) {
+		return Texts.key(variation.getTranslatableComponent(modifier.getTranslationKey()));
+	}
+
+	public static Text getModifierName(KeyModifier modifier) {
+		return getModifierName(modifier, Variation.NORMAL);
+	}
+
 	public static Text addModifiers(Text text, KeyMapping keybind) {
 		if (isAmecsApiLoaded) {
-			KeyModifiers modifiers = ((IKeyBinding)keybind).amecs$getKeyModifiers();
+			Text t = Texts.literal("");
 
-			if (modifiers.getAlt())
-				text.append(Texts.literal("Alt").format(Formatting.YELLOW)).append(" + ");
-			if (modifiers.getControl())
-				text.append(Texts.literal("Control").format(Formatting.YELLOW)).append(" + ");
-			if (modifiers.getShift())
-				text.append(Texts.literal("Shift").format(Formatting.YELLOW)).append(" + ");
+			for (KeyModifier modifier : getKeyModifiers(keybind)) {
+				t.
+					append(getModifierName(modifier)).
+					append(" + ");
+			}
+
+			text = t.append(text);
 		}
 
 		return text;
