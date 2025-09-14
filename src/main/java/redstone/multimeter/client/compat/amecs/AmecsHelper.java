7421d39a24113e6cfac8970d0249daa3007b6499
Space Walker <spacedoesrs@gmail.com>
2025-09-14 12:24:31 +0200

    re-introduce Amecs compat

--- src/main/java/redstone/multimeter/client/compat/amecs/AmecsHelper.java
+++ src/main/java/redstone/multimeter/client/compat/amecs/AmecsHelper.java
@@ -1,5 +1,5 @@
 package redstone.multimeter.client.compat.amecs;
-/*
+
 import java.util.Collection;
 import java.util.Collections;
 import java.util.LinkedList;
@@ -10,15 +10,14 @@ import de.siphalor.amecs.api.KeyModifiers;
 import de.siphalor.amecs.impl.ModifierPrefixTextProvider.Variation;
 
 import net.fabricmc.loader.api.FabricLoader;
-*/
-import net.minecraft.client.KeyMapping;
-//import net.minecraft.network.chat.Component;
-import net.minecraft.network.chat.MutableComponent;
 
-//import redstone.multimeter.util.TextUtils;
+import net.minecraft.client.KeyMapping;
+
+import redstone.multimeter.client.gui.text.Text;
+import redstone.multimeter.client.gui.text.Texts;
 
 public class AmecsHelper {
-/*
+
 	private static boolean isAmecsApiLoaded = FabricLoader.getInstance().isModLoaded("amecsapi");
 
 	public static Collection<KeyModifier> getKeyModifiers(KeyMapping keybind) {
@@ -39,22 +38,26 @@ public class AmecsHelper {
 		return modifiers;
 	}
 
-	public static Component getModifierName(KeyModifier modifier, Variation variation) {
-		return MutableComponent.create(variation.getTranslatableText(modifier.getTranslationKey()));
+	public static Text getModifierName(KeyModifier modifier, Variation variation) {
+		return Texts.key(variation.getTranslatableComponent(modifier.getTranslationKey()));
 	}
 
-	public static Component getModifierName(KeyModifier modifier) {
+	public static Text getModifierName(KeyModifier modifier) {
 		return getModifierName(modifier, Variation.NORMAL);
 	}
-*/
-	public static MutableComponent addModifiers(MutableComponent text, KeyMapping keybind) {
-		/*if (isAmecsApiLoaded) {
+
+	public static Text addModifiers(Text text, KeyMapping keybind) {
+		if (isAmecsApiLoaded) {
+			Text t = Texts.literal("");
+
 			for (KeyModifier modifier : getKeyModifiers(keybind)) {
-				text.
-					append(TextUtils.formatKey(getModifierName(modifier))).
+				t.
+					append(getModifierName(modifier)).
 					append(" + ");
 			}
-		}*/
+
+			text = t.append(text);
+		}
 
 		return text;
 	}
